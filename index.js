dc89fe2d5c1113de81d56a90afa61b3e8061aacc
heeeyueee <heeeyueee@163.com>
2020-11-16 11:05:07 +0800

    鼠标按下画点，抬起停下

--- index.js
+++ index.js
@@ -2,17 +2,32 @@
 let canvas = document.getElementById("stockGraph");
 canvas.width = document.documentElement.clientWidth;
 canvas.height = document.documentElement.clientHeight;
-console.log(canvas);
 let ctx = canvas.getContext("2d");
+let paintFlag = false;
 //开始画点
+// canvas.onmousemove = (e) => {
+//     //画矩形
+//     console.log(e);
+//     ctx.fillStyle = "rgb(200,0,0)";
+//     ctx.fillRect(e.clientX, e.clientY, 5, 5);
+//     //画圆
+//     ctx.beginPath();
+//     ctx.arc(e.clientX, e.clientY, 5, 0, 2 * Math.PI);
+//     ctx.fillStyle = "black";
+//     ctx.fill();
+// }
+//改进——鼠标按下画点，抬起停下
+canvas.onmousedown = () => {
+    paintFlag = true;
+}
 canvas.onmousemove = (e) => {
-    //画矩形
-    console.log(e);
-    ctx.fillStyle = "rgb(200,0,0)";
-    ctx.fillRect(e.clientX, e.clientY, 5, 5);
-    //画圆
-    ctx.beginPath();
-    ctx.arc(e.clientX, e.clientY, 5, 0, 2 * Math.PI);
-    ctx.fillStyle = "black";
-    ctx.fill();
+    if (paintFlag === true) {
+        ctx.beginPath();
+        ctx.arc(e.clientX, e.clientY, 5, 0, 2 * Math.PI);
+        ctx.fillStyle = "black";
+        ctx.fill();
+    }
+}
+canvas.onmouseup = () => {
+    paintFlag = false;
 }
