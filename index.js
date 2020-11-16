dbea83654d936ecee61febad3ef3b5e0ca1138f8
heeeyueee <heeeyueee@163.com>
2020-11-16 13:43:46 +0800

    手机画板

--- index.js
+++ index.js
@@ -4,7 +4,9 @@ canvas.width = document.documentElement.clientWidth;
 canvas.height = document.documentElement.clientHeight;
 let ctx = canvas.getContext("2d");
 let paintFlag = false;
-//开始画点
+ctx.translate(0.5, 0.5);
+ctx.strokeStyle = 'none';
+//1.开始画点
 // canvas.onmousemove = (e) => {
 //     //画矩形
 //     console.log(e);
@@ -16,18 +18,47 @@ let paintFlag = false;
 //     ctx.fillStyle = "black";
 //     ctx.fill();
 // }
-//改进——鼠标按下画点，抬起停下
-canvas.onmousedown = () => {
-    paintFlag = true;
-}
-canvas.onmousemove = (e) => {
-    if (paintFlag === true) {
+//2.改进——鼠标按下画点，抬起停下
+// canvas.onmousedown = () => {
+//     paintFlag = true;
+// }
+// canvas.onmousemove = (e) => {
+//     if (paintFlag === true) {
+//         ctx.beginPath();
+//         ctx.arc(e.clientX, e.clientY, 5, 0, 2 * Math.PI);
+//         ctx.fillStyle = "black";
+//         ctx.fill();
+//     }
+// }
+// canvas.onmouseup = () => {
+//     paintFlag = false;
+// }
+//3.在手机上运行——手机画板
+//先判断是不是触屏设备
+let isTouchDevice = "ontouchstart" in document.documentElement;
+console.log(isTouchDevice);
+if (isTouchDevice === true) {
+    canvas.addEventListener('touchmove', function (e) {
         ctx.beginPath();
-        ctx.arc(e.clientX, e.clientY, 5, 0, 2 * Math.PI);
+        ctx.arc(e.changedTouches[0].clientX, e.changedTouches[0].clientY, 10, 0, 2 * Math.PI);
         ctx.fillStyle = "black";
+        ctx.stroke();
         ctx.fill();
+    })
+} else {
+    canvas.onmousedown = () => {
+        paintFlag = true;
     }
-}
-canvas.onmouseup = () => {
-    paintFlag = false;
+    canvas.onmousemove = (e) => {
+        if (paintFlag === true) {
+            ctx.beginPath();
+            ctx.arc(e.clientX, e.clientY, 10, 0, 2 * Math.PI);
+            ctx.fillStyle = "black";
+            ctx.stroke();
+            ctx.fill();
+        }
+    }
+    canvas.onmouseup = () => {
+        paintFlag = false;
+    }
 }
